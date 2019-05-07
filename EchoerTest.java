4bb06112a1a9ba6466fbd3d62ca71fb34574be3c
junghwa <wjdghk466@naver.com>
2019-05-07 23:51:14 +0900

    added output "hello!"

--- EchoerTest.java
+++ EchoerTest.java
@@ -21,7 +21,7 @@ public class EchoerTest {
 				while(true) {
 					String sen = in.nextLine();		// 메시지를 한 줄(개행문자,엔터)을 기준으로 입력을 받음
 					System.out.println(sen);
-					if(sen.equals("끝"))
+					if(sen.equals("종료"))
 						break;				// "끝"이라는 메시지를 입력받으면 반복문을 종료함
 				}
 			}
@@ -30,5 +30,7 @@ public class EchoerTest {
 		e.start();
 		e.echo();
 		e.stop();
+
+		System.out.println("Hello!");
 	}
 }
